b8e71bc5156817a4a9ca88dc5faff0b4c90257a8
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-08-25 14:02:10 +0400

    changed something?

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,4 @@ exec { 'fix--for-nginx':
   command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
+
