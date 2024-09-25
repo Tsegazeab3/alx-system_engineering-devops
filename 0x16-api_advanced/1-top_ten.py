d916db5a0d85664a420145a51f7297f3b25c68ce
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-09-26 02:28:26 +0400

    done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,3 @@
-"""#!/usr/bin/python3
-Script to print hot posts on a given Reddit subreddit.
-
-import requests
-
-
-def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/tsegazeab422)"
-    }
-
-
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False)
-
-    if response.status_code == 404:
-        print("None")
-        return
-    data = response.json().get("data").get("children")
-    top_10_posts = "\n".join(post.get("data").get("title") for post in data)
-    print(top_10_posts)
-"""
 #!/usr/bin/python3
 """
 Script to print hot posts on a given Reddit subreddit.
