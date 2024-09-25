4ab9aa1024c1e37ae9daab9573f8df4c9c4bc289
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-09-26 02:17:54 +0400

    this might work

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,17 +14,14 @@ def top_ten(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
 
-   params = {
-       "limit": 10
-   }
 
-    response = requests.get(url, headers=headers,params=params
+    response = requests.get(url, headers=headers,
                             allow_redirects=False)
 
     if response.status_code == 404:
         print("None")
         return
+    data = response.json().get("data").get("children")
+    top_10_posts = "\n".join(post.get("data").get("title") for post in data)
+    print(top_10_posts)
 
-    results = response.json().get("data").get("children")
-
-    [print(c.get("data").get("title")) for c in results.get("children")]
