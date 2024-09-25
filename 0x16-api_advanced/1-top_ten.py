21ff402927156c0482e888e2632737586f8343b5
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-09-26 01:59:49 +0400

    something

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,17 +14,17 @@ def top_ten(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
 
-    params = {
-        "limit": 10
-    }
+   params = {
+       "limit": 10
+   }
 
-    response = requests.get(url, headers=headers, params=params,
+    response = requests.get(url, headers=headers,params=params
                             allow_redirects=False)
 
     if response.status_code == 404:
         print("None")
         return
 
-    results = response.json().get("data")
+    results = response.json().get("data").get("children")
 
     [print(c.get("data").get("title")) for c in results.get("children")]
